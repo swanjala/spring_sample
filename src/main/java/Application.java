c7a010de436040c52155806504d93e7d4081f8d8
swanjala <wanjala.sam.s@gmail.com>
2019-04-23 06:35:52 +0300

    (update): Implement a setter injecttion
    - add an application context in main app

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,17 @@
 import com.service.CustomerService;
 import com.service.ICustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String [] args){
-        ICustomerServiceImpl customerService = new CustomerService();
+
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService customerService = applicationContext.getBean("customerService",CustomerService.class);
+
 
         System.out.println(customerService.findAll().get(0).getFirstName());
     }
