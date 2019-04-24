0e2e10a5a786ebbbd1f0be5c899e5a53baee25c9
swanjala <wanjala.sam.s@gmail.com>
2019-04-24 04:21:46 +0300

    (update): add a bean annotation to get customer service

--- src/main/java/com/AppConfig.java
+++ src/main/java/com/AppConfig.java
@@ -1,4 +1,23 @@
 package com;
 
+import com.sample.repository.ICustomerRepository;
+import com.service.CustomerService;
+import com.service.ICustomerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // class level annotation
 public class AppConfig {
+
+    /* method level annotion, to show what this
+    instance returns is an instance of a bean and available
+    for us to in our Spring application.
+     */
+
+    @Bean(name="customerService")// bean naming convention, like a variable
+    public ICustomerServiceImpl getCustomerService() {
+        /* Implementing a concretion and returning the interface
+        * */
+        return new CustomerService();
+    }
 }
